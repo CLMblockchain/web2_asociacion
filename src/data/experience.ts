eb80a8f2b2f7c8f3084a071d5e39340da110eb51
Asociacion Blockchain Castilla la Mancha <desarrollo@clmblockchain.org>
2024-10-07 13:32:08 +0200

    Revert "Cargamos el contenido de las entradas desde contentful e incluimos react"

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -1,21 +1,55 @@
-import { getEntries, getTagName } from "../libs/contentful/contentful-client";
+import type { ExperienceProps } from "../types/experience.props";
 
-export async function getExperience() {
-  const entries = await getEntries();
-  const data =entries.map((entry) => {
-    const tags = entry.metadata.tags.map(async (tag) => {
-      const tagName = await getTagName(tag.sys.id);
-      return tagName;
-    });
-    return{
-      dates: entry.fields.date,
-      title: entry.fields.title,
-      company: entry.fields.company,
-      companyUrl: entry.fields.companyUrl ?? "",
-      description: entry.fields.description.content[0].content[0].value,
-      technologies: tags,
-      logo: entry?.fields?.logo?.fields?.file?.url,
-    }
-  });
-  return data;
-}
+export const EXPERIENCE: ExperienceProps[] = [
+  {
+    dates: "03/10/2024 20h",
+    title: "Reunión General",
+    company: "Discord",
+    companyUrl: "",
+    description:
+      "En nuestra próxima Reunión General del próximo Miércoles 3 de Octubre definiremos un equipo de trabajo dedicado a la DAO 🔥 ",
+    technologies: [
+      "Reunión General",
+      "Asociacón",
+      "Discord",
+    ],
+    logo: "/images/logo_clm.webp",
+  },
+  {
+    dates: "24/09/2024",
+    title: "Primera reunión DAO CLM",
+    company: "Discord",
+    companyUrl: "https://drive.google.com/file/d/1LG8fCTNRLq0L_SGw1zrzIdtWSxtheD7_/view?usp=sharing",
+    description:
+      "Primera Sesión de creación de la DAO para la Asociación completada! 🎯 Queremos agradecer a María del Sagrario Navarro Lérida e Ignacio Baixauli Quiles por ayudarnos ayer y aportar toda su visión y experiencia. Ahora toca bajar toda esa información a un documento colaborativo y empezar a trabajarlo entre todos 💪🏻 Somos una asociación abierta a todo el mundo, por lo que si estás interesado en formar parte de la definición de la DAO es el momento 😎",
+    technologies: [
+      "DAO",
+      "Discord",
+      "Presentación",
+    ],
+    logo: "/images/DAO_logo.webp",
+  },
+  {
+    dates: "19/08/2024",
+    title: "Seguna Kickoff",
+    company: "Zoom",
+    companyUrl: "",
+    description:
+      "Buenas comunidad! Segunda Reunión General completada 😎 Ya tenemos los equipos de trabajo creados y asignados, ahora toca currar para sacar los objetivos del roadmap que nos hemos puesto a corto plazo 🚀 echadle un vistazo de como va cambiando el tablero de organización 🙌🏻 También estrenamos Discord para una comunicación más estructurada y organización de los eventos como el de ayer por ese nuevo canal 🎯 os animamos a todos a participar en la próxima reu dentro de 2-3 semanas, son bienvenidas todas las ideas y hay muchas formas de colaborar, os esperamos!",
+    technologies: [
+      "Finalizada",
+      "Community Call",
+    ],
+    logo: "/images/logo_clm.webp",
+  },
+  {
+    dates: "23/07/2024",
+    title: "Primera Kickoff completada 😎",
+    company: "Zoom",
+    companyUrl: "",
+    description:
+      "La semana pasada tuvimos nuestra primera reunión como asociación donde estuvimos presentándonos y viendo las primeras acciones que acometer, como formalizarnos oficialmente como Asociación 💪🏻 Os dejamos por aquí el board que seguimos por encima para que le echéis un vistazo, por supuesto sentiros libres de participar y aportar cualquier idea o colaboración que se os ocurra 🙌🏻 Os esperamos por nuestro Telegram!",
+    technologies: ["Community Call",],
+    logo: "/images/logo_clm.webp",
+  },
+];
